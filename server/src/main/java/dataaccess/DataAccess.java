d8365fd23889845ded11dcd5b18af185dd53eba7
Devin Halversen <dhalver6@gmail.com>
2025-11-17 20:34:51 -0700

    Added the required things for interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,24 @@
+package dataaccess;
+
+import model.UserData;
+import model.GameData;
+import model.AuthData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+    void clear() throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+}
