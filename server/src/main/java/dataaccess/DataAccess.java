d4190ef40a3c14f5c02e0137e19363cc8c3e4d6e
Devin Halversen <dhalver6@gmail.com>
2025-11-17 21:38:42 -0700

    storage

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -16,7 +16,7 @@ public interface DataAccess {
     void createAuth(AuthData auth) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
-    
+
     int createGame(String gameName) throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
     Collection<GameData> listGames() throws DataAccessException;
---
+++ server/src/main/java/dataaccess/InMemoryDataAccess.java
@@ -0,0 +1,83 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryDataAccess implements DataAccess {
+
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextGameID = 1;
+
+    @Override
+    public void clear() {
+        users.clear();
+        auths.clear();
+        games.clear();
+        nextGameID = 1;
+    }
+
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+
+    @Override
+    public void createAuth(AuthData auth) {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        auths.remove(authToken);
+    }
+
+
+    @Override
+    public int createGame(String gameName) {
+        int id = nextGameID++;
+        var game = new GameData(id, null, null, gameName, new ChessGame());
+        games.put(id, game);
+        return id;
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game not found");
+        }
+        games.put(game.gameID(), game);
+    }
+}
