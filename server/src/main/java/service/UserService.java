0b8ac9fd12fbf797474357b696431c22a4d94b98
Devin Halversen <dhalver6@gmail.com>
2025-11-18 16:43:10 -0700

    small fix

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -5,8 +5,11 @@ import dataaccess.DataAccessException;
 import model.UserData;
 import model.AuthData;
 
-private final DataAccess dataAccess;
+public class UserService {
 
-public UserService(DataAccess dataAccess) {
-    this.dataAccess = dataAccess;
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
 }
