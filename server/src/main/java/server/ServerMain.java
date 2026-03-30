5e79a5b1007c10145f5f95626e014b4f4bc93397
Devin Halversen <dhalver6@gmail.com>
2026-03-30 10:15:31 -0600

    fix server structure, add ServerMain, fix pom

---
+++ server/src/main/java/server/ServerMain.java
@@ -0,0 +1,9 @@
+package server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        Server server = new Server();
+        int port = server.run(8080);
+        System.out.println("240 Chess Server running on port " + port);
+    }
+}
