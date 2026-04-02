b4632bf436518ad9b9e3d1efdce367ee0b7d378e
Devin Halversen <dhalver6@gmail.com>
2026-04-02 16:38:36 -0600

    added updategame for MySQL

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -301,6 +301,27 @@ public class MySqlDataAccess implements DataAccess {
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
-        
+        String statement = """
+                UPDATE games
+                SET white_username = ?, black_username = ?, game_name = ?, game_json = ?
+                WHERE game_id = ?
+                """;
+
+        String gameJson = new Gson().toJson(game.game());
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, game.whiteUsername());
+            ps.setString(2, game.blackUsername());
+            ps.setString(3, game.gameName());
+            ps.setString(4, gameJson);
+            ps.setInt(5, game.gameID());
+
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            throw new DataAccessException("Unable to update game: " + e.getMessage());
+        }
     }
 }
