0c874f383dde5604bda5fd9d3cf694e81ccd23e8
Devin Halversen <dhalver6@gmail.com>
2026-04-03 20:10:41 -0600

    fixed

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class MySqlDataAccess implements DataAccess {
 
     public MySqlDataAccess() throws DataAccessException {
+        DatabaseManager.createDatabase();
         configureDatabase();
     }
 
